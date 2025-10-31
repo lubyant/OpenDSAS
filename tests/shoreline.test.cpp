e75bd3b2f4afa2088619ee90f1e6dad192841323
lubyant <blu38@wisc.edu>
2025-10-30 23:39:02 -0500

    wip

--- tests/shoreline.test.cpp
+++ tests/shoreline.test.cpp
@@ -3,3 +3,9 @@
 #include <gtest/gtest.h>
 using namespace dsas;
 #define TOL 1e-4
+
+TEST(TestShoreline, test_load_shorelines_shp) {
+  const std::filesystem::path shoreline_shp_path{std::string(TEST_DATA_DIR) +
+                                                 "/sample_shorelines.geojson"};
+  load_shorelines_shp(shoreline_shp_path, "date");
+}
